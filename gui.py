f5dde40db16dbf49f9812c764e4c525c10ab392c
Sara-Pesso <s.pessognelli@outlook.com>
2026-08-10 12:40:54 -0400

    update app -- still needs revision

--- gui.py
+++ gui.py
@@ -2,12 +2,11 @@ import tkinter as tk
 from tkinter import scrolledtext 
 from secret_santa import *
 
-exclusions = flip_dict_entries(spouses)
 names = [key for key, _ in exclusions.items()]
 
 def secret_santa_outputs(output_widget):
     # Generate the list of items
-    secret_santa_list = secret_santa_generator(names, spouses) # names and spouses from secret_santa.py
+    secret_santa_list = secret_santa_generator(exclusions) # names and spouses from secret_santa.py
 
     # Clear previous content in the Text widget
     output_widget.config(state="normal") # Make the widget editable temporarily
@@ -20,10 +19,10 @@ def secret_santa_outputs(output_widget):
     # Make the widget read-only again to prevent user editing
     output_widget.config(state="disabled")
 
-def display_spouse_dict(spouses_list_widget):
-    # Check for new spouse pairs!!!!
-    gift_giver = spouse_name1_box.get()
-    ex = spouse_name2_box.get()
+def display_exclusions_dict(exclusions_list_widget):
+    # Check for new exclusions pairs!!!!
+    gift_giver = exclusions_name1_box.get()
+    ex = exclusions_name2_box.get()
 
     if gift_giver and ex:
         # String scrubbing
@@ -32,24 +31,25 @@ def display_spouse_dict(spouses_list_widget):
 
         exclusions.update({gift_giver:ex})
         # Empty the textboxes
-        spouse_name1_box.delete(0, tk.END)
-        spouse_name2_box.delete(0, tk.END)
+        exclusions_name1_box.delete(0, tk.END)
+        exclusions_name2_box.delete(0, tk.END)
 
     # Clear previous content in the Text widget
-    spouses_list_widget.config(state="normal") # Make the widget editable temporarily
-    spouses_list_widget.delete('1.0', tk.END) 
+    exclusions_list_widget.config(state="normal") # Make the widget editable temporarily
+    exclusions_list_widget.delete('1.0', tk.END) 
     
     for key, value in exclusions.items():
-        spouses_list_widget.insert(tk.END, f"{key}:{value}" + "\n")
+        exclusions_list_widget.insert(tk.END, f"{key}:{value}" + "\n")
 
     # Make the widget read-only again to prevent user editing
-    spouses_list_widget.config(state="disabled")
+    exclusions_list_widget.config(state="disabled")
 
 
 def display_all_names(names_text_widget):
     # Check for added names
     new_name = add_name_box.get()
     if new_name:
+        exclusions.update({new_name:[]})
         names.append(new_name)
         # Empty the textboxes
         add_name_box.delete(0, tk.END)
@@ -73,27 +73,27 @@ screen_height = root.winfo_screenheight()
 # Set the window geometry to match the screen size
 root.geometry(f"{screen_width}x{screen_height}+0+0")
 
-### Displaying ALL names of people in secret santa pool, regardless of spousal relationship
+### Displaying ALL names of people in secret santa pool, regardless of exclusion relationship
 names_textbox = scrolledtext.ScrolledText(root, wrap=tk.WORD, state="disabled", height=10, padx=5, pady=5)
 names_textbox.pack(pady=10, fill=tk.BOTH, expand=True)
 
 add_name_box = tk.Entry(root, width=30)
 add_name_box.pack(pady=10)
 
-display_button = tk.Button(root, text="Display/Add Names", command=lambda: display_all_names(names_textbox))
+display_button = tk.Button(root, text="Display Names", command=lambda: display_all_names(names_textbox))
 display_button.pack(pady=10)
 
-### Displaying the names and spouse pairings
-spouse_textbox = scrolledtext.ScrolledText(root, wrap=tk.WORD, state="disabled", height=10, padx=5, pady=5)
-spouse_textbox.pack(pady=10, fill=tk.BOTH, expand=True)
+### Displaying the names and exclusions pairings
+exclusions_textbox = scrolledtext.ScrolledText(root, wrap=tk.WORD, state="disabled", height=10, padx=5, pady=5)
+exclusions_textbox.pack(pady=10, fill=tk.BOTH, expand=True)
 
-spouse_name1_box = tk.Entry(root, width=30)
-spouse_name1_box.pack(pady=10)
+exclusions_name1_box = tk.Entry(root, width=30)
+exclusions_name1_box.pack(pady=10)
 
-spouse_name2_box = tk.Entry(root, width=30)
-spouse_name2_box.pack(pady=10)
+exclusions_name2_box = tk.Entry(root, width=30)
+exclusions_name2_box.pack(pady=10)
 
-display_button = tk.Button(root, text="Display/Add Exclusions", command=lambda: display_spouse_dict(spouse_textbox))
+display_button = tk.Button(root, text="Display/Add Exclusions", command=lambda: display_exclusions_dict(exclusions_textbox))
 display_button.pack(pady=10)
 
 ### OUTPUT!
